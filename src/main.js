0b4a119cfef4335432351e1c263c1e92ae2289a1
阳葵 <yaakua@126.com>
2017-09-14 17:48:04 +0800

    增加bootstrap样式

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/css/bootstrap-theme.css"
 
 Vue.config.productionTip = false
 
