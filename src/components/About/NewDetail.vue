983be2db415e9ac360205fd49aeaca09ceef4ffa
enhui <enhui.rao@newtouch.cn>
2017-09-18 17:22:21 +0800

    add newDetail

---
+++ src/components/About/NewDetail.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="aboutNewDetail">
+    newDetail
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'newDetail'
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.aboutSupport{
+  margin: 60px auto;
+}
+</style>
