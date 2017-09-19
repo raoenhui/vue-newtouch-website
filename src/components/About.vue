0af099d02c045ce3e2fa6efa8d99a74202d3f30c
enhui <enhui.rao@newtouch.cn>
2017-09-19 15:05:51 +0800

    about 修改

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,31 +1,41 @@
 <template>
   <div class="about">
-      <div id="carousel1" class="carousel slide" data-ride="carousel">
+    <div class="carousel slide">
 
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner" role="listbox">
-          <div class="item active">
-            <img class="marginCenter" :src="srcBg">
-          </div>
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <h1>{{srcTit}}</h1>
+          <img class="marginCenter" :src="srcBg">
         </div>
       </div>
+    </div>
 
     <div class="nav-controller clearfix">
 
       <div class="about-nav">
         <ul role="tablist">
-          <router-link to="/about/newtouch"><div class="about-nav-line" @click="showBgFun('newtouch')">日新以致远</div></router-link>
-          <router-link to="/about/news"><div class="about-nav-line" @click="showBgFun('news')">企业动态</div></router-link>
-          <router-link to="/about/support"><div class="about-nav-line" @click="showBgFun('support')">公益支持</div></router-link>
-          <router-link to="/about/contact"><div class="about-nav-line" @click="showBgFun('contact')">联系我们</div></router-link>
-          <router-link to="/about/join"><div class="about-nav-line" @click="showBgFun('join')">加入我们</div></router-link>
+          <router-link to="/about/newtouch">
+            <div class="about-nav-line" @click="showBgFun('newtouch')">日新以致远</div>
+          </router-link>
+          <router-link to="/about/news">
+            <div class="about-nav-line" @click="showBgFun('news')">企业动态</div>
+          </router-link>
+          <router-link to="/about/support">
+            <div class="about-nav-line" @click="showBgFun('support')">公益支持</div>
+          </router-link>
+          <router-link to="/about/contact">
+            <div class="about-nav-line" @click="showBgFun('contact')">联系我们</div>
+          </router-link>
+          <router-link to="/about/join">
+            <div class="about-nav-line" @click="showBgFun('join')">加入我们</div>
+          </router-link>
         </ul>
         <div class="tab-content">
           <router-view></router-view>
         </div>
       </div>
     </div>
-
   </div>
 
 </template>
@@ -33,46 +43,67 @@
 <script>
   import jQuery from "jquery";
 
+  const InfoObj = {
+    'newtouch': '日新以致远',
+    'news': '企业动态',
+    'support': '公益支持',
+    'contact': '联系我们',
+    'join': '加入我们'
+  }
+
   export default {
     name: 'main',
     data () {
       return {
         //背景图片
-        srcBg: require('../assets/about/newtouch_bg.png')
+        srcBg: require('../assets/about/newtouch_bg.png'),
+        //背景文字
+        srcTit: ''
       }
     },
     methods: {
       showBgFun: function (path) {
         this.srcBg = require(`../assets/about/${path}_bg.png`);
+        this.srcTit = InfoObj[path];
       }
     },
     mounted: function () {
 
-        var path=this.$route.path.split('/')[2];
-        if(path){
-          this.srcBg = require(`../assets/about/${path}_bg.png`);
+      var path = this.$route.path.split('/')[2];
+      if (path) {
+        this.srcBg = require(`../assets/about/${path}_bg.png`);
+        this.srcTit = InfoObj[path];
 
-        }
+      }
     }
   }
 </script>
 
+<!--<script type="text/javascript" src="http://api.map.baidu.com/api?v=2.0&ak=d33xUOlYyOEP6pRiTDcaM4rc"></script>-->
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .about .carousel h1 {
+    position: absolute;
+    color: #fff;
+    z-index: 1;
+    width: 100%;
+    text-align: center;
+    top: 35%;
+  }
 
   /*关于我们导航样式
   */
-
-  .about-nav{
+  .about-nav {
     margin-top: -43px;
     border-left: none;
     text-align: center;
   }
 
-  .about .about-nav ul>a{
+  .about .about-nav ul > a {
     display: inline-block;
   }
-  .about .about-nav .about-nav-line{
+
+  .about .about-nav .about-nav-line {
     line-height: 42px;
     margin: 0px;
     display: inline-block;
@@ -82,14 +113,15 @@
     border-right: solid 0.5px #fff;
     margin-right: -5px;
     padding: 0 20px;
-    color:#fff
+    color: #fff
   }
-  .about .about-nav .about-nav-line:hover{
+
+  .about .about-nav .about-nav-line:hover {
     cursor: pointer;
   }
 
-  .tab-content{
-    line-height:30px;
+  .tab-content {
+    line-height: 30px;
     text-align: center;
   }
 
@@ -101,11 +133,11 @@
     margin-left: 9%;
   }
 
-  .tab-pane ul li.active a{
+  .tab-pane ul li.active a {
     color: #fff;
   }
 
-  .tab-pane ul li a{
+  .tab-pane ul li a {
     color: #000;
   }
 
--- src/components/About/Newtouch.vue
+++ src/components/About/Newtouch.vue
@@ -464,12 +464,33 @@
 </template>
 
 <script>
+  //  import {MP} from './map.js'
   export default {
     name: 'newtouch',
     methods: {
       showTab: function (i) {
         $(`.aboutNewtouch .tab-pane ul>li:nth-child(${i})`).addClass('active').siblings().removeClass('active')
         $(`.aboutNewtouch .tab-pane-con-tab:nth-child(${i})`).removeClass('hide').siblings().addClass('hide')
+      },
+      showMap:function(){
+        // 百度地图API功能
+//      MP().then(function(BMap){
+//          var map = new BMap.Map("allmap");
+//          var point = new BMap.Point(116.331398,39.897445);
+//          map.centerAndZoom(point,12);
+//          // 创建地址解析器实例
+//          var myGeo = new BMap.Geocoder();
+//          // 将地址解析结果显示在地图上,并调整地图视野
+//          myGeo.getPoint("上海市浦东新区峨山路91弄98号陆家嘴软件园1号楼", function(point){
+//            if (point) {
+//              map.centerAndZoom(point, 16);
+//              map.addOverlay(new BMap.Marker(point));
+//            }else{
+//              alert("您选择地址没有解析到结果!");
+//            }
+//          }, "上海市");
+//        }
+//      );
       }
     }
   }
