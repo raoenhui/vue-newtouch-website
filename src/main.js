94b0ae4da778683be275f5c4fa5998f09798da2a
enhui <enhui.rao@newtouch.cn>
2017-09-22 17:14:35 +0800

    主站修改

--- src/main.js
+++ src/main.js
@@ -1,19 +1,14 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-
 import 'bootstrap/css/bootstrap.min.css'
 import "font-awesome/css/font-awesome.css";
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'bootstrap/js/bootstrap.min.js'
-
 import $ from 'jquery'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-var vm =new Vue({
+window.VUE = new Vue({
   el: '#app',
   router,
   template: '<App/>',
